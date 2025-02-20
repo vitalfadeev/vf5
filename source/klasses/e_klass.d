1924f10b2082ef3d47c79e67223d641f11495c7a
Vital Fadeev <vital.fadeev@gmail.com>
2025-02-20 19:12:06 +0700

    upd

--- source/klasses/e_klass.d
+++ source/klasses/e_klass.d
@@ -585,6 +585,7 @@ set_content_image (UTree* doc_t, UTree* e_t, TString[] values) {
 
     if (values.length) {
         e.content.image.src = values[0].s;
+        e.content.image.ptr = null;
     }
 }
 
